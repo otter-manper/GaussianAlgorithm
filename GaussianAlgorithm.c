d8f71b2193c3bdc849494a974f83ca72b4fa05c8
otter-manper <151945404+otter-manper@users.noreply.github.com>
2024-06-18 14:19:41 -0300

    Add files via upload

--- GaussianAlgorithm.c
+++ GaussianAlgorithm.c
@@ -3,37 +3,26 @@
 #include <math.h>
 
 
-struct dimension
+typedef struct 
 {
     int row;
     int col;
-};
+}dimension; 
 
-struct fraction
-{
-    int numer;
-    int denom;
-};
-
-struct fraction multifrac(struct fraction, struct fraction);
-struct fraction addifrac(struct fraction, struct fraction);
-struct fraction reciprocal(struct fraction);
-struct fraction reduce(struct fraction);
-
-void add(double **matrix, struct dimension space, int row1, int row2, double multiple);
-void swap(double **matrix, struct dimension space, int row1, int row2);
-void multiply(double **matrix, struct dimension space, int row1, double multiple);
-void gaussianSolve(double **matrix, struct dimension space);
-void printMatrix(double **matrix, struct dimension space);
+void add(double **matrix,  dimension space, int row1, int row2, double multiple);
+void swap(double **matrix,  dimension space, int row1, int row2);
+void multiply(double **matrix,  dimension space, int row1, double multiple);
+void gaussianSolve(double **matrix,  dimension space);
+void printMatrix(double **matrix,  dimension space);
 
 main(){
-    struct dimension space;
+    dimension space;
     printf("Enter the row amount: ");
-    scanf("%d", &space.row);
+    scanf("%d", &(space.row));
     printf("Enter the column amount: ");
-    scanf("%d", &space.col);
+    scanf("%d", &(space.col));
     
-    double * * matrix = malloc(sizeof(double) * space.col);
+    double * * matrix = malloc(sizeof(double*) * space.col);
 
     for(int i = 0;i < space.col; i++){
         matrix[i] = malloc(sizeof(double) * space.row);
@@ -48,7 +37,7 @@ main(){
         }
     }
 
-    printf("%lf\n",matrix[0][0]);
+    
 
     printMatrix(matrix, space);
 
@@ -62,22 +51,27 @@ main(){
         free(matrix[i]);
     }
     free(matrix);
+    
 }
 
-void gaussianSolve(double **matrix, struct dimension space){
+void gaussianSolve(double **matrix, dimension space){
+
+
+    //ROW ECHELON FORM
     for(int n = 0;n < space.row;n++){//repeat solve for each row
+        
         int i = 0;
         for(i = n;i < space.col;i++){//go down...
-            printf("%lf",matrix[n][i]);
-            if(matrix[n][i] != 0){//...column n and find a non-zero value in row i
+            
+            if(matrix[i][n] != 0){//...column n and find a non-zero value in row i
                 swap(matrix, space, i, n);//swap row i and row n
                 break;
             }
         }//end loop. Either non-zero value found and swapped and now in row n, or i has reached the end of the column (i = space.col)
         i = n;
-        if (i != space.col)//only do the rest if there is space down the column
+        if (i != space.row-1)//only do the rest if there is space down the column
         {
-            printf("\n==\nI = %d\n==\n",i);
+            
             double multiple = 1.0/matrix[i][n];
             multiply(matrix, space, n, multiple);//set first non-zero number in row n to 1
             for (int j = n + 1; j < space.col; j++)//for each row below row n, add a value such where the values in column n become zero
@@ -86,22 +80,53 @@ void gaussianSolve(double **matrix, struct dimension space){
             }
             
         }//finished of column n, now column n has some number of 0 below a 1, or all 0s.
-        
+        else{
+            double multiple = 1.0/matrix[i][n];
+            multiply(matrix, space, n, multiple);
+        }
     }
 
-    //Reduced Row Echelon Form:
+    //************************************************************************************************** */
+    //
+    //          MATRIX IS NOW IN ROW ECHELON FORM
+    //
+    //************************************************************************************************** */
 
-    //TODO
+    printMatrix(matrix, space);
 
+    printf("\nReducing...\n\n");
+
+    //REDUCED ROW ECHELON FORM
+    for(int n = 1;n < space.row;n++){//repeat solve for each leading 1 in row
+        
+        int i;
+        for (i = 1; i < space.col && matrix[i][n] == 0; i++)//find that leading 1 (continue down the matrix until you see a non-zero number, which will be 1 because REF)
+        {}//The for loop only needs to get the value of i
+
+        if (i == space.col)
+        {
+            i = i-1;
+        }
+        
+
+        if (matrix[i][n] != 0)
+        {
+            
+            for (int j = i-1; j >= 0; j--)//Go up column and add to set all values above to 0
+            {
+                add(matrix, space, j, n, -(matrix[n][j])/(matrix[n][n]));
+            }
+        }
+        
+            
+    }
+  
+
+    return;
     
 }
-//TODO:
-//Change to fractions
-//GUI
-//Ncurses
 
-
-void printMatrix(double **matrix, struct dimension space){
+void printMatrix(double **matrix,  dimension space){
     printf("\n");
     for (int i = 0; i < space.row; i++)
     {
@@ -113,11 +138,12 @@ void printMatrix(double **matrix, struct dimension space){
     }
 }
 
-void swap(double **matrix, struct dimension space, int row1, int row2){
+void swap(double **matrix,  dimension space, int row1, int row2){
 
-    printMatrix(matrix, space);
-
-    printf("\nSwapping...\n");
+    if(row1 == row2){
+        return;
+    }
+    
 
     for (int i = 0; i < space.col; i++)
     {
@@ -128,30 +154,26 @@ void swap(double **matrix, struct dimension space, int row1, int row2){
         matrix[i][row2] = temp;
     }
     
-    printMatrix(matrix, space);
+    
 
     
 }
 
-void add(double **matrix, struct dimension space, int row1, int row2, double multiple){
+void add(double **matrix,  dimension space, int row1, int row2, double multiple){
 
-    printMatrix(matrix, space);
-
-    printf("\nAdding...%lf\n", multiple);
+    
 
     for (int i = 0; i < space.col; i++)
     {
         matrix[i][row1] += matrix[i][row2] * multiple;
     }
 
-    printMatrix(matrix, space);
+  
 }
 
-void multiply(double **matrix, struct dimension space, int row1, double multiple){
+void multiply(double **matrix,  dimension space, int row1, double multiple){
 
-    printMatrix(matrix, space);
-
-    printf("\nMultiplying...%lf\n", multiple);
+    
 
     if (multiple == 0 || multiple == INFINITY || multiple == -INFINITY)
     {
@@ -165,47 +187,6 @@ void multiply(double **matrix, struct dimension space, int row1, double multiple
         matrix[i][row1] *= multiple;
     }
 
-    printMatrix(matrix, space);
+   
     
-}
-
-struct fraction multifrac(struct fraction tar1, struct fraction tar2){
-    
-    struct fraction goal;
-
-    goal.denom = tar1.denom * tar2.denom;
-
-    goal.numer = tar1.numer * tar2.numer;
-    
-    return goal;
-}
-struct fraction addifrac(struct fraction, struct fraction);
-
-
-struct fraction reciprocal(struct fraction target){
-    struct fraction goal;
-    goal.numer = target.denom;
-    goal.denom = target.numer;
-    return goal;
-}
-
-struct fraction reduce(struct fraction target){
-    int gcf = 1;
-
-    int bed = min(target.numer,target.denom);
-
-    for (int i = 0; i < bed; i++)
-    {
-        if (target.numer % i == 0 && target.denom % i == 0)
-        {
-            gcf = i;
-        }
-        
-    }
-
-    struct fraction goal;
-
-    goal.numer = target.numer/gcf;
-    goal.denom = target.denom/gcf;
-    return goal;
 }
